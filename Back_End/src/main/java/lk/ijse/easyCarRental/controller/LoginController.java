d116ea85e8c7f34d05227696b0612716769b939d
Akila Piumal <akilapiumal16@gmail.com>
2023-02-25 15:52:02 +0530

    ResponseUtil Added

--- Back_End/src/main/java/lk/ijse/easyCarRental/controller/LoginController.java
+++ Back_End/src/main/java/lk/ijse/easyCarRental/controller/LoginController.java
@@ -2,6 +2,7 @@ package lk.ijse.easyCarRental.controller;
 
 import lk.ijse.easyCarRental.dto.LoginDTO;
 import lk.ijse.easyCarRental.service.LoginService;
+import lk.ijse.easyCarRental.util.ResponseUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,7 +15,8 @@ public class LoginController {
     LoginService service;
 
     @PostMapping
-    public void saveLogin(@RequestBody LoginDTO dto){
+    public ResponseUtil saveLogin(@RequestBody LoginDTO dto){
         service.saveLogin(dto);
+        return new ResponseUtil("200",dto.toString()+" Registered",null);
     }
 }
